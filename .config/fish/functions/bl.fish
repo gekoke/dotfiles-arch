62b6a09002651d64b107c510aba7fcea97143ac4
gekoke <63151928+gekoke@users.noreply.github.com>
2022-04-08 01:07:24 +0300

    Fix bw alias

--- .config/fish/functions/bl.fish
+++ .config/fish/functions/bl.fish
@@ -1,6 +1,4 @@
 function bl
-    set BW_EMAIL (cat ~/.secrets/pw_manager_email)
-    set -g BW_SESSION (bw login $BW_EMAIL --raw --passwordfile ~/.secrets/pw_manager)
-    set -e BW_EMAIL
+    bw login (cat ~/.secrets/pw_manager_email $BW_EMAIL) --raw --passwordfile ~/.secrets/pw_manager
 end
 

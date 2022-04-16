0b72c89f38c9da40673da45b2fc6d6ff87d5a762
gekoke <63151928+gekoke@users.noreply.github.com>
2022-04-17 01:46:39 +0300

    Add less colors

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -10,6 +10,24 @@ function _configure_default_programs
     set -U SUDO_EDITOR "lvim"
     set -U BROWSER "brave"
     set -U AUR_HELPER "paru"
+    set -U PAGER "less -r"
+    set -U MANPAGER "less -r"
+end
+
+function less_colors
+    set -Ux LESS_TERMCAP_mb (tput bold; tput setaf 2)
+    set -Ux LESS_TERMCAP_md (tput bold; tput setaf 6)
+    set -Ux LESS_TERMCAP_me (tput sgr0)
+    set -Ux LESS_TERMCAP_so (tput bold; tput setaf 3; tput setab 4)
+    set -Ux LESS_TERMCAP_se (tput rmso; tput sgr0)
+    set -Ux LESS_TERMCAP_us (tput smul; tput bold; tput setaf 7)
+    set -Ux LESS_TERMCAP_ue (tput rmul; tput sgr0)
+    set -Ux LESS_TERMCAP_mr (tput rev)
+    set -Ux LESS_TERMCAP_mh (tput dim)
+    set -Ux LESS_TERMCAP_ZN (tput ssubm)
+    set -Ux LESS_TERMCAP_ZV (tput rsubm)
+    set -Ux LESS_TERMCAP_ZW (tput rsupm)
+    set -Ux LESS_TERMCAP_ZO (tput ssupm)
 end
 
 function _configure_configurations
@@ -18,6 +36,7 @@ function _configure_configurations
     set -gx SECURELOCK_NO_COMPOSITE 1
     set -gx GCM_CREDENTIAL_STORE "gpg"
     set -gx NOTES_DIRECTORY "$HOME/.notes"
+    less_colors
 end
 
 function _configure_environment

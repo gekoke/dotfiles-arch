c7d303c81ee9425c475f21878fbc8765623f90af
gekoke <63151928+gekoke@users.noreply.github.com>
2022-05-04 20:04:41 +0300

    Misc bump

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,48 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light))
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
@@ -65,18 +22,30 @@
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+
+;; User details
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Font
+(setq doom-font                (font-spec :family "IosevkaTerm" :size 16 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "IosevkaTerm" :size 16 :weight 'medium))
+
+;; Theme
+(setq doom-theme 'doom-gruvbox)
+(setq display-line-numbers-type 'relative)
+
+;; Keybindings
+(map! :leader "x" #'kill-current-buffer)
+
+;; Modeline
+(setq display-time-default-load-average nil)
+(setq display-time-format "%H:%M")
+(display-time-mode 1)
 
 ;; Org
 (setq org-directory "~/.org/")
+(setq org-agenda-files '("~/.org/agenda.org"))
 
 ;; Idris2
 (use-package! idris2-mode
@@ -84,6 +53,7 @@
   :config
 
   (setq idris2-semantic-source-highlighting nil)
+  (setq idris2-stay-in-current-window-on-compiler-error t)
 
   (after! lsp-mode
     (add-to-list 'lsp-language-id-configuration '(idris2-mode . "idris2"))
@@ -92,11 +62,10 @@
       (make-lsp-client
         :new-connection (lsp-stdio-connection "idris2-lsp")
         :major-modes '(idris2-mode)
-        :server-id 'idris2-lsp))
-    )
-    (setq lsp-semantic-tokens-enable t)
-    (add-hook 'idris2-mode-hook #'lsp!)
-)
+        :server-id 'idris2-lsp)))
+
+  (setq lsp-semantic-tokens-enable t)
+  (add-hook 'idris2-mode-hook #'lsp!))
 
 (after! company
   (setq company-global-modes
@@ -105,5 +74,4 @@
        help-mode
        gud-mode
        vterm-mode
-       idris2-repl-mode))  ;; Fix Idris2 REPL crashing on input
-)
+       idris2-repl-mode)))  ;; Fix Idris2 REPL crashing on input
